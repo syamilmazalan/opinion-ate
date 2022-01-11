c8d250764e15b1f6bd201f25ee987c6f1398d7da
Syamil <smazalan@yahoo.com>
2022-01-11 16:41:12 +0800

    add new restaurant form

---
+++ tests/e2e/specs/creating-a-restaurant.spec.js
@@ -0,0 +1,34 @@
+describe('Creating a Restaurant', () => {
+  it('allows adding restaurants', () => {
+    const restaurantId = 27;
+    const restaurantName = 'Sushi Place';
+
+    cy.server({force404: true});
+
+    cy.route({
+      method: 'GET',
+      url: 'https://outside-in-dev-api.herokuapp.com/6oaG120jyn1p5yNH14SIZ5VdRRwXf33k/restaurants',
+      response: [],
+    });
+
+    cy.route({
+      method: 'POST',
+      url: 'https://outside-in-dev-api.herokuapp.com/6oaG120jyn1p5yNH14SIZ5VdRRwXf33k/restaurants',
+      response: {
+        id: restaurantId,
+        name: restaurantName,
+      },
+    }).as('addRestaurant');
+
+    cy.visit('/');
+
+    cy.get('[placeholder="Add Restaurant"').type(restaurantName);
+    cy.contains('Add').click();
+
+    cy.wait('@addRestaurant').its('requestBody').should('deep.equal', {
+      name: restaurantName,
+    });
+
+    cy.contains(restaurantName);
+  });
+});
